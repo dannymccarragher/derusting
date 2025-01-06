a2c14f125a5f9b92e32d2e7c7e5c94400e06142e
Danny McCarragher <dannymccarragher@gmail.com>
2025-01-05 22:24:27 -0800

    Partial Completion of String Practice

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
@@ -25,15 +27,26 @@ public class StringPractice {
 
     // Iterate over the characters of the string, printing each one on a separate line
 
+    for(int i = 0; i < stringLen; i++){
+      char ch = string.charAt(i);
+      System.out.println(ch);
+    }
     
 
     // Create an ArrayList of Strings and assign it to a variable
+    ArrayList<String> list = new ArrayList<>();
 
     // Add multiple strings to the List (OK to do one-by-one)
 
+    list.add("First");
+    list.add("Second");
+    list.add("Third");
+
     // Join all of the strings in the list together into a single string separated by commas
     // Use a built-in method to achieve this instead of using a loop
+    String joinResult = String.join(",", list);
 
+    System.out.println(joinResult);
     // Check whether two strings are equal
 
     /*
